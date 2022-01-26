3adf52f622b98d16a60ef1b458c25bbf8efb7488
Davi Freitas <df828316@gmail.com>
2022-01-26 17:55:36 -0300

    req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,26 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data; // array species
 function countAnimals(animal) {
-  const { species } = data; // array species
   if (animal === undefined) {
-    const totalSpecie = species.map((obj) => {
-      const newObj = {};
+    let newObj = {};
+    species.map((obj) => {
       newObj[obj.name] = obj.residents.length;
       return newObj;
     });
-    return totalSpecie;
+    return newObj;
   }
   const findSpecie = species.find(({ name }) => animal.specie === name); // obj da specie
   const { residents } = findSpecie; // array dos residents
   if (animal.sex !== undefined) {
     const filterSex = residents.filter(({ sex }) => animal.sex === sex);
-    const totalSpecie = filterSex.length;
-    return totalSpecie;
+    return filterSex.length;
   }
-  const totalSpecie = residents.length;
-  return totalSpecie;
+  return residents.length;
 }
 
-console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals());
 
 module.exports = countAnimals;
