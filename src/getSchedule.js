3504b597e417f7af9fc40f27c3b9eed8fe26add3
Davi Freitas <df828316@gmail.com>
2022-01-27 16:40:28 -0300

    corrigido lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-const { hours } = data; // array dos dias
-const { species } = data; // array species
+// const { hours } = data; // array dos dias
+// const { species } = data; // array species
 // const animal = species.map((obj) => obj.name); // nome dos animais
 // const days = Object.keys(hours); // nome dia da semana
 
