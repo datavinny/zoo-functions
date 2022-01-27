0e62f7d4969cd9778cd24a7c1293dc1fc58f30c3
Davi Freitas <df828316@gmail.com>
2022-01-27 16:32:45 -0300

    req9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species } = data;
+const { employees } = data;
+
+function getOldestFromFirstSpecies(employeeId) {
+  const employee = employees.find(({ id }) => id === employeeId);
+  const { responsibleFor } = employee;
+  const filterResponsibleFor = species.find(
+    (element, index) => element.id === responsibleFor[0],
+  );
+  console.log(filterResponsibleFor);
+  const { residents } = filterResponsibleFor;
+  const boxOldest = residents.reduce((previousValue, currentValue) => {
+    if (currentValue.age >= previousValue.age) {
+      return currentValue;
+    }
+    return previousValue;
+  });
+  const oldest = [];
+  oldest.push(boxOldest.name);
+  oldest.push(boxOldest.sex);
+  oldest.push(boxOldest.age);
+  return oldest;
 }
+
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { hours } = data; // array dos dias
+const { species } = data; // array species
+// const animal = species.map((obj) => obj.name); // nome dos animais
+// const days = Object.keys(hours); // nome dia da semana
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // if (scheduleTarget === undefined) {
+  // }
 }
 
 module.exports = getSchedule;
