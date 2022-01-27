617580f78a1b1ef154cb5353dee5bbe4a0e1c274
Davi Freitas <df828316@gmail.com>
2022-01-27 16:37:37 -0300

    corrigido lint req9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,61 +1,10 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
-const spl = { NE: '' }; // speciesPerLocation
+// const { species } = data;
+// const spl = { NE: '' }; // speciesPerLocation
 
 function getAnimalMap(options) {
-  if (
-    options === undefined ||
-    !Object.keys(options).length ||
-    options.includeNames !== undefined && options.includeNames === false ||
-    options.sex !== undefined
-  ) {
-    spl.NE = species
-      .filter(({ location }) => location === 'NE')
-      .map((element) => element.name);
-    spl.NW = species
-      .filter(({ location }) => location === 'NW')
-      .map((element) => element.name);
-    spl.SE = species
-      .filter(({ location }) => location === 'SE')
-      .map((element) => element.name);
-    spl.SW = species
-      .filter(({ location }) => location === 'SW')
-      .map((element) => element.name);
-    return spl;
-  }
-  if (options.includeNames !== undefined && options.includeNames === true) {
-    spl.NE = species
-      .filter(({ location }) => location === 'NE')
-      .map(({ name, residents }) => {
-        const newObj = {};
-        newObj[name] = residents.name;
-        return newObj;
-      });
-    spl.NW = species
-      .filter(({ location }) => location === 'NW')
-      .map(({ name, residents }) => {
-        const newObj = {};
-        newObj[name] = residents;
-        return newObj;
-      });
-    spl.SE = species
-      .filter(({ location }) => location === 'SE')
-      .map(({ name, residents }) => {
-        const newObj = {};
-        newObj[name] = residents;
-        return newObj;
-      });
-    spl.SW = species
-      .filter(({ location }) => location === 'SW')
-      .map(({ name, residents }) => {
-        const newObj = {};
-        newObj[name] = residents;
-        return newObj;
-      });
-    return spl;
-  }
-  return 'erro';
+
 }
 
 console.log(getAnimalMap({ includeNames: true }));
