eeff84321bd4b91e89318ad285a2ee3813fb31e0
Davi Freitas <df828316@gmail.com>
2022-01-27 17:46:54 -0300

    corrigido lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -5,27 +5,27 @@ const data = require('../data/zoo_data');
 
 // const array = ['NE', 'NW', 'SE', 'SW'];
 function getAnimalMap(options) {
-  // if (options === undefined || !Object.keys(options).length ||options.includeNames !== undefined && options.includeNames === false ||options.sex !== undefined) {
-  //   for (let index = 0; index < array.length; index += 1) {
-  //     spl[array[index]] = species.filter(({ location }) => location === array[index]).map((element) => element.name);
-  //   }
-  //   return spl;
-  // }
-  // if (options.includeNames) {
-  //   for (let index = 0; index < array.length; index += 1) {
-  //     spl[array[index]] = species.filter(({ location }) => location === array[index]).map(({ name, residents }) => {
-  //       const newObj = {};
-  //       arrResidents = [];
-  //       arrResidents.push(residents[index].name);
-  //       newObj[name] = arrResidents;
-  //       console.log(residents[index].name)
-  //       return newObj;
-  //     }); 
-  //   }
-  //   return spl;
-  // }
-  // return 'erro';
+
 }
+// if (options === undefined || !Object.keys(options).length ||options.includeNames !== undefined && options.includeNames === false ||options.sex !== undefined) {
+//   for (let index = 0; index < array.length; index += 1) {
+//     spl[array[index]] = species.filter(({ location }) => location === array[index]).map((element) => element.name);
+//   }//   return spl;
+// }
+// if (options.includeNames) {
+//   for (let index = 0; index < array.length; index += 1) {
+//     spl[array[index]] = species.filter(({ location }) => location === array[index]).map(({ name, residents }) => {
+//       const newObj = {};
+//       const arrResidents = [];
+//       arrResidents.push(residents[index].name);
+//       newObj[name] = arrResidents;
+//       console.log(residents[index].name)
+//       return newObj;
+//     });
+//   }
+//   return spl;
+// }
+// return 'erro';
 
 // console.log(getAnimalMap({ includeNames: true }));
 
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -4,32 +4,7 @@ const data = require('../data/zoo_data');
 // const { species } = data;
 
 function getEmployeesCoverage(obj) {
-  // if(obj === undefined) {
 
-  // }
-  // const employee = employees.find(({ firstName, lastName, id }) => firstName === obj.name || lastName === obj.name || id === obj.id);
-  // if (employee === undefined) {
-  //   throw new Error('Informações inválidas');
-  // }
-  // const { firstName, lastName, id } = employee;
-  // const fullName = `${firstName} ${lastName}`;
-  // const { responsibleFor } = employee;
-  // const speciesEmployee = [];
-  // const locationsEmployee = [];
-  // species.forEach(({ id, name, location }) => {
-  //   for (let index = 0; index < responsibleFor.length; index += 1) {
-  //     if (id === responsibleFor[index]) {
-  //       speciesEmployee.push(name);
-  //       locationsEmployee.push(location);
-  //     }
-  //   }
-  // });
-  // const result = {};
-  // result.id = id;
-  // result.fullName = fullName;
-  // result.species = speciesEmployee;
-  // result.locations = locationsEmployee;
-  // return result;
 }
 
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }));
