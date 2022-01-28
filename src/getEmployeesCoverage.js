6dabb762d5a3d9420fb31befa5c287112f892363
Davi Freitas <df828316@gmail.com>
2022-01-28 10:08:29 -0300

    req10 com erro no lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,14 +1,72 @@
 const data = require('../data/zoo_data');
 
-// const { employees } = data;
-// const { species } = data;
+const { employees } = data;
+const { species } = data;
+
+const noParam = () => {
+  // const allSpecies = [];
+  // const allLocations = [];
+  const infoAllEmployees = [];
+  employees.forEach(({ firstName, lastName, id, responsibleFor }) => {
+    const allSpecies = [];
+    const allLocations = [];
+    species.forEach(({ id, name, location }) => {
+      responsibleFor.forEach((element) => {
+        if (id === element) {
+          allSpecies.push(name);
+          allLocations.push(location);
+        }
+      });
+    });
+    const infoEmployees = { id };
+    infoEmployees.fullName = `${firstName} ${lastName}`;
+    infoEmployees.species = allSpecies;
+    infoEmployees.locations = allLocations;
+    infoAllEmployees.push(infoEmployees);
+  });
+  return infoAllEmployees;
+};
+
+const checkId = (obj) => {
+  const employee = employees.find(
+    ({ firstName, lastName, id }) =>
+      firstName === obj.name || lastName === obj.name || id === obj.id,
+  );
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return employee;
+};
+
+const getEmployeeInfo = (obj) => {
+  const employee = checkId(obj);
+  const { firstName, lastName, id, responsibleFor } = employee;
+  const speciesEmployee = [];
+  const locationsEmployee = [];
+  species.forEach(({ id, name, location }) => {
+    responsibleFor.forEach((element) => {
+      if (id === element) {
+        speciesEmployee.push(name);
+        locationsEmployee.push(location);
+      }
+    });
+  });
+  const infoEmployee = { id };
+  infoEmployee.fullName = `${firstName} ${lastName}`;
+  infoEmployee.species = speciesEmployee;
+  infoEmployee.locations = locationsEmployee;
+  return infoEmployee;
+};
 
 function getEmployeesCoverage(obj) {
-
+  if (obj === undefined) {
+    return noParam();
+  }
+  return getEmployeeInfo(obj);
 }
 
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 // console.log(getEmployeesCoverage({ name: 'Spry' }));
-// console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 // const { hours } = data; // array dos dias
 // const { species } = data; // array species
 // const animal = species.map((obj) => obj.name); // nome dos animais
-// const days = Object.keys(hours); // nome dia da semana
+// const days = []; // nome dia da semana utilizar um array com os dia
 
 function getSchedule(scheduleTarget) {
   // if (scheduleTarget === undefined) {
